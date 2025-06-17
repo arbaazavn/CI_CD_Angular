92faaffe57ad84aa44b24306486c9d3542471a94
arbaaz <murehman@avanceon.ae>
2025-06-17 09:46:41 +0500

    refactor: reorganize ESLint configuration for improved structure and readability

--- eslint.config.js
+++ eslint.config.js
@@ -1,45 +1,45 @@
-import { FlatCompat } from '@eslint/eslintrc';
 import js from '@eslint/js';
+import { FlatCompat } from '@eslint/eslintrc';
 
 const compat = new FlatCompat({
   recommendedConfig: js.configs.recommended,
-  allConfig: js.configs.all
+  allConfig: js.configs.all,
 });
 
 export default [
   ...compat.extends(
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
-    'plugin:@angular-eslint/recommended',
+    'plugin:@angular-eslint/recommended'
   ),
-  ...compat.overrides([
-    {
-      files: ['*.ts'],
+  {
+    files: ['*.ts'],
+    languageOptions: {
       parserOptions: {
         project: ['tsconfig.json'],
         createDefaultProgram: true,
       },
-      rules: {
-        '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
-        '@typescript-eslint/explicit-function-return-type': 'off',
-        '@typescript-eslint/no-explicit-any': 'warn',
-        '@angular-eslint/component-class-suffix': ['error', { suffixes: ['Component'] }],
-        '@angular-eslint/directive-class-suffix': ['error', { suffixes: ['Directive'] }],
-        '@angular-eslint/no-host-metadata-property': 'off',
-        'eqeqeq': ['error', 'always'],
-        'no-console': ['warn', { allow: ['warn', 'error'] }],
-        'prefer-const': 'error',
-      },
     },
-    // Uncomment and adjust for template rules if needed
-    // {
-    //   files: ['*.html'],
-    //   extends: ['plugin:@angular-eslint/template/recommended'],
-    //   rules: {
-    //     '@angular-eslint/template/no-any': 'warn',
-    //     '@angular-eslint/template/eqeqeq': 'error',
-    //     '@angular-eslint/template/banana-in-box': 'error',
-    //   },
-    // },
-  ]),
+    rules: {
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@angular-eslint/component-class-suffix': ['error', { suffixes: ['Component'] }],
+      '@angular-eslint/directive-class-suffix': ['error', { suffixes: ['Directive'] }],
+      '@angular-eslint/no-host-metadata-property': 'off',
+      eqeqeq: ['error', 'always'],
+      'no-console': ['warn', { allow: ['warn', 'error'] }],
+      'prefer-const': 'error',
+    },
+  },
+  // Uncomment for template rules
+  // {
+  //   files: ['*.html'],
+  //   plugins: { '@angular-eslint/template': require('@angular-eslint/template') },
+  //   rules: {
+  //     '@angular-eslint/template/no-any': 'warn',
+  //     '@angular-eslint/template/eqeqeq': 'error',
+  //     '@angular-eslint/template/banana-in-box': 'error',
+  //   },
+  // },
 ];
